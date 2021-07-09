e06d0ca1682d919f6f3b9e79bcfd47f0be1776f5
sridharmkumar <sridhar.muthukumar.in@gmail.com>
2021-07-09 13:14:29 +0530

    scripts for data pipeline creation

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# sqoop-hive-data-pipeline
+# pike
+
+Customer analytics based on timestamp.
+
+Using Sqoop incremental append & Hive managed & external tables for staging & target.
+
+Execution Instruction:
+- Checkout & execute init.sh
+- Create cron job to import data on 21:30 (local time)
+<code>
+crontab -e
+30 21 * * * sh /home/cloudera/exec.sh
+</code>
---
+++ sqlscript.sql
@@ -0,0 +1,3 @@
+create database pike;
+use pike;
+create table pike.web_customer (id int(100),username varchar(100),sub_port varchar(100),host varchar(100),date_time varchar(100),hit_count_val_1 varchar(100),hit_count_val_2 varchar(100),hit_count_val_3 varchar(100),timezone varchar(100),method varchar(100),`procedure` varchar(100),value varchar(100), sub_product varchar(100), web_info varchar(1000),status_code varchar(100));
